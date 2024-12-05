13042565fb051ff6bdc1ada9efb16aa61c214187
FIDYFIRE <fidyfire@gmail.com>
2024-12-05 18:23:08 +0300

    à propos

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,25 @@
+<script setup>
+</script>
+
+<template>
+    <div id="about-me">
+        <h2 class="text-highlight-1 section-title">Qui suis-je ?</h2>
+        <p class="default-text">Je suis un <span class="text-highlight-2">développeur web</span> passionné par
+            la création de sites web et d'applications web innovantes.
+        </p>
+    </div>
+</template>
+
+<style scoped>
+    #about-me {
+        margin-top: 80px;
+    }
+
+    .default-text {
+        text-align: justify;
+    }
+
+    .default-text span{
+        font-weight: bold;
+    }
+</style>
